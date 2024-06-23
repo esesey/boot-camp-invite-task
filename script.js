199636dc12c38e85196076d5e47ec0fe8224d4b5
esesey <esesey@yandex.ru>
2024-06-23 03:15:44 +0300

    first version

--- script.js
+++ script.js
@@ -1,95 +1,65 @@
-let pageViewed = false
+let cards = []
+const searchInput = document.getElementById('search-input');
 
 async function fetchCards() {
     fetch('https://rickandmortyapi.com/api/character')
     .then(response => response.json())
-    .then(json => json.results.forEach(appendCards))
+    .then(json => {
+      cards = json.results
+      cards.forEach(appendCards)})
+    .catch(error => console.error('error fetching data:', error))
+    console.log('just fetched');
+}
+
+function updateSearch() {
+  const filter = searchInput.value.toLowerCase()
+  const filteredCards = cards.filter(card =>
+    card.name.toLowerCase().includes(filter))
+  const main = document.getElementById('main');
+
+  filteredCards.forEach(appendCards)
+
+  console.log('just updated');
 }
 
 function composeCard(cardData) {
-    // Если ничего не передано, ничего не возвращаем
-    if (!cardData) {
-      console.error('not found cardData')
-      return;
-    }
-  
-    // Обращаемся к старому шаблону
-    const template = document.getElementById('character-card')
-    
-    if (!template) {
-      console.error('not found card template')
-      return;
-    }
-
-    // Клонируем
-    const card = template.content.cloneNode(true)
-  
-    // Получаем нужную информацию
-    const { name, image, species, status, location } = cardData
-  
-    // Добавляем соответствующие тексты и числа
-    card.querySelector('h2').textContent = 'Name: ' + name
-    card.querySelector('img').src = image    
-    card.querySelector('p1').textContent = 'Species: ' + species
-    card.querySelector('p2').textContent = 'Status: ' + status
-    card.querySelector('p3').textContent = 'Location: ' + location.name
-  
-    // Возвращаем созданный элемент
-    return card
+  if (!cardData) {
+    console.error('not found card data')
+    return;
   }
+
+  const template = document.getElementById('character-card')
+
+  if (!template) {
+    console.error('not found card template')
+    return;
+  }
+  
+  const card = template.content.cloneNode(true)
+  const { name, image, species, status, location } = cardData
+  
+  card.querySelector('h2').textContent = name
+  card.querySelector('img').src = image    
+  card.querySelector('p1').textContent =  species
+  card.querySelector('p2').textContent = status
+  card.querySelector('p3').textContent =  location.name
+  
+  return card
+}
 
 function appendCards(cardData) {
-    // Если данных нет, ничего не делаем
-    if (!cardData) return
-  
-    // Храним ссылку на элемент
-    const main = document.getElementById('main')
-  
-    // Превращает данные в HTML-элемент
-    const cardNode = composeCard(cardData)
-  
-    // Добавляем созданный элемент в <main>
-    main.append(cardNode)
+  if (!cardData) {
+    console.error('not found card data')
+    return;
   }
+  
+  const main = document.getElementById('main')
+  const cardNode = composeCard(cardData)
+  
+  main.append(cardNode)
+}
 
-async function checkPosition() {
-    // Высота документа и экрана
-    const height = document.body.offsetHeight
-    const screenHeight = window.innerHeight
-  
-    // Сколько пикселей уже проскроллили
-    const scrolled = window.scrollY
-  
-    // Порог
-    const threshold = height - screenHeight / 4
-  
-    // Низ экрана относительно страницы
-    const position = scrolled + screenHeight
-  
-    if (position >= threshold) {
-      await fetchCards()
-    }
-  }
-
-function throttle(callee, timeout) {
-    let timer = null
-
-    return function perform(...args) {
-      if (timer) return
-  
-      timer = setTimeout(() => {
-        callee(...args)
-  
-        clearTimeout(timer)
-        timer = null
-      }, timeout)
-    }
-  }  
-
-  ;(() => {
-    window.addEventListener('scroll', throttle(checkPosition, 250))
-    window.addEventListener('resize', throttle(checkPosition, 250))
-    window.addEventListener('firast_fetch', function(){
-      if (!pageViewed) {fetchCards(); pageViewed=true}
-    })
-  })()
+document.addEventListener('DOMContentLoaded', function() {
+  searchInput.addEventListener('input', updateSearch);
+  fetchCards();
+});
